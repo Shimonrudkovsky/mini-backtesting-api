5e6f8de247dfb973ea5c9aa912f60db9a6287d1d
Simon <semen.rudkovskiy@vkpay.com>
2025-02-15 23:34:31 +0100

    rename main.py to be able to launch module

--- mini_backtesting_api.py
+++ mini_backtesting_api.py
@@ -19,7 +19,7 @@ async def root():
 
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("mini_backtesting_api:app", host="0.0.0.0", port=8080, reload=True)
 
 
 if __name__ == "__main__":

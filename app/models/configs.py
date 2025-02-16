92f1e7e1d184f63dbedc6d774bd01a3d410073a3
Simon <semen.rudkovskiy@vkpay.com>
2025-02-16 19:23:12 +0100

    s3 renamed to configs in models and AppConfig added

--- app/models/configs.py
+++ app/models/configs.py
@@ -5,3 +5,9 @@ class S3Config(BaseModel):
     url: str
     aws_access_key_id: str
     aws_secret_access_key: str
+    bucket_name: str
+
+
+class AppConfig(BaseModel):
+    port: int
+    s3: S3Config
